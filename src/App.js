7c9628021f549e4497d6ce35c990c3401a25b6bd
Rhonnie Al <rhonnieallan@protonmail.com>
2023-01-23 17:02:24 +0200

    Fix build error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import './App.css';
-import { findBestLinkStation } from './components/LinkStationFinder'
+import findBestLinkStation from './components/LinkStationFinder.js'
 
 /* function GetLinkedStationtoConnect(x, y) {
   let allLinkStation = [
