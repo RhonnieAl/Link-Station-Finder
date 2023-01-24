1005a233783f5ccda15e35f4373c094502624c7b
Rhonnie Al <rhonnieallan@protonmail.com>
2023-01-24 05:05:34 +0200

    Refactor code & Fix error

--- src/components/LinkStationFinder.js
+++ src/components/LinkStationFinder.js
@@ -3,61 +3,56 @@ This module finds the best Link Station Router (Stations) with the highest power
 for a set of given devices (Points)
 */
 
-function findBestLinkStation(point, stations) {
-    // Store highest power is the variable bestPower. 
-    // Lowest (out of range) power possble is 0 
-    let bestPower = 0;
-    let bestStation = null;
+function GetBestLinkStation(x, y) {
+    let Stations = [
+        [0, 0, 10],
+        [20, 20, 5],
+        [10, 0, 12],
+    ];
 
-    for (let i = 0; i < stations.length; i++) {
-        // Get distance between each Link Station (station) and Device (point)
-        let distance = Math.sqrt(Math.pow(stations[i][0] - point[0], 2)
-         + Math.pow(stations[i][1] - point[1], 2));
+    // Store highest power in the variable "power". 
+    // Lowest (out of range) power possble is defined as 0 
+    let power = 0;
+    let preferedStation_data = [];
+
+    // Max number of iterations set to total number of stations
+    for (let i = 0; i < Stations.length; i++) {
+        let currentStation = Stations[i];
+
+        // Get distance between each Link Station and Device point (x, y) on 2D plane
+        let distance = Math.sqrt(
+            Math.pow(currentStation[0] - x, 2) +
+            Math.pow(currentStation[1] - y, 2) 
+        );
         
-         // Create undefined container variable
-         let power;
-         
-        if (distance > stations[i][2]) {
-            // Define lowest power
-            power = 0;
+        // Find if distance is greater than Station's reach
+        if (distance > currentStation[2]) {
+            continue;
         } else {
             // Get power of a Link Station to a given device
-            power = Math.pow(stations[i][2] - distance, 2);
-        }
-        if (power > bestPower) {
-            // Get highest powered value if above 0 of given device (point) 
-            // Get coordinates of station with highest power to given device (point)
-            bestPower = power;
-            bestStation = stations[i];
+            let currrent_power = Math.pow(currentStation[2] - distance, 2);
+            
+            // Retrun the highest powered Link Station to Device, if any
+            if (currrent_power > power) {
+                power = currrent_power;
+                preferedStation_data = currentStation;
+            }
         }
     }
-
     // Output message to user
-    if (bestPower > 0) {
-        return `Best link station for point ${point[0]},${point[1]} is ${bestStation[0]},${bestStation[1]} with power ${bestPower}`;
+    if (power > 0) {
+        return `Best link station for point (${x},${y}) is (${preferedStation_data[0]},${preferedStation_data[1]}) with power ${power}`;
     } else {
-        return `No link station within reach for point ${point[0]},${point[1]}`;
-    }
+        return `No link station within reach for point (${x},${y})`;
+       }
+    
 }
 
-// List of all available Link Stations (Add more Station if installed)
-let stations = [
-    [0, 0, 10],
-    [20, 20, 5],
-    [10, 0, 12]
-];
+    // Find most suitable Link Station for all given Device points
+    GetBestLinkStation(0, 0);
+    GetBestLinkStation(100, 100);
+    GetBestLinkStation(15, 10);
+    GetBestLinkStation(18, 18);
 
-// List of all available Devices (Add more Station if installed)
-let points = [
-    [0, 0],
-    [100, 100],
-    [15, 10],
-    [18, 18]
-];
 
-// Get best Link Station for all given Devices (points)
-for (let i = 0; i < points.length; i++) {
-    findBestLinkStation(points[i], stations);
-}
-
-export default findBestLinkStation;
+    export default GetBestLinkStation;
